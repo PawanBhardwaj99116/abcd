5858acc51d484c20a5d52ea93aaba150daeda928
PawanBhardwaj99116 <pawansharma12349@gmail.com>
2022-06-06 16:56:04 +0530

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,28 +2,37 @@
 
 async function getdata(){
     try{
+
         let res = await fetch(`https://masai-vouchers-api.herokuapp.com/api/vouchers`);
         let data = await res.json();
-        console.log('This is the data',data)
+        console.log('This is the data',data);
     }
     
     catch(err){
-      console.log("error",err)
+      console.log("error",err);
     }
 
 }
 getdata();
 
-function savedata(){
+document.getElementById("submit").addEventListener("click",addItem);
 
+function savedata(event){
+event.preventDefault();
   let name1 = document.getElementById("name").value 
   let email1 = document.getElementById("email").value 
   let image1 = document.getElementById("image").value 
   let amount1 = document.getElementById("amount").value 
 
-  let myobj{
-    
+  let myobj={
+ name :name1,
+ email :email1,
+ image :image1,
+ amount : Number(amount1),
+  };
 
-  }
+   arr.push(myobj);
+localStorage.setItem("user",JSON.stringify(arr));
+  
 
 }
